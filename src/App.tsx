4c17daee61fe2dbd8afa9147dd9918e5ca9db07b
abdulbari149 <abdulbari131103@gmail.com>
2021-10-24 11:18:20 +0500

    Routing Enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useRef, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import config from "./config";
+import { Layout } from "./components";
 
-function App() {
+import { useTitle } from "./hooks";
+
+interface Props {}
+
+const App: React.FC<Props> = () => {
+  const [pageTitle, setPageTitle] = useState("Loading...");
+  useTitle(pageTitle);
+
+  function dynamicImport(pageName: string, title: string) {
+    setPageTitle(title);
+    return React.lazy(() => import(`./pages/${pageName}`));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Suspense fallback={<div> Loading ... </div>}>
+          <Switch>
+            {config.pages.map(
+              ({ id, name, title, pageProps, ...routeProps }) => (
+                <Route
+                  key={id}
+                  {...routeProps}
+                  render={(props) => {
+                    const RenderComponent = dynamicImport(name, title);
+                    return <RenderComponent />;
+                  }}
+                />
+              )
+            )}
+          </Switch>
+        </Suspense>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
